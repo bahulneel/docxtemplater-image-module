75a2f1c318b4deca7a4e3897be48da22992a6b61
Edgar Hipp <hipp.edg@gmail.com>
2014-12-02 13:51:50 +0100

    remove unnecessary stuff in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,6 @@ var coffee= require('gulp-coffee');
 var concat= require('gulp-concat');
 var uglify= require('gulp-uglify');
 var spawn = require('child_process').spawn;
-// var livereload = require('gulp-livereload');
-// var browserify = require('gulp-browserify');
 var server=null;
 
 var config={uglify:false}
@@ -18,26 +16,6 @@ var paths = {
 	testDirectory:__dirname+'/test',
     js:'js/'
 };
-
-// gulp.task('browserify', function() {
-//     browserified=gulp.src(__dirname+'/test.test.js')
-//         .pipe(browserify({}))
-
-//     browserified
-//         .pipe(gulp.dest(__dirname+'/browser/'))
-
-//     // Single entry point to browserify
-//     browserified=gulp.src(__dirname+'/examples/main.js')
-//         .pipe(browserify({}))
-
-//     browserified
-//         .pipe(uglify())
-//         .pipe(rename('main.min.js'))
-//         .pipe(gulp.dest(__dirname+'/browser'))
-
-//     browserified
-//         .pipe(gulp.dest(__dirname+'/browser/'))
-// });
 
 gulp.task('allCoffee', function () {
 	gulp.src(paths.coffee)
@@ -59,9 +37,6 @@ gulp.task('watch', function () {
 		.pipe(watch(function(files) {
 			var f=files.pipe(coffee({bare:true}))
 				.pipe(gulp.dest(paths.js))
-			// gulp.run('browserify');
-			// gulp.run('jasmine');
-			// gulp.run('livereload');
 			return f;
 		}));
 
@@ -78,8 +53,6 @@ gulp.task('coffeeTest', function() {
 	a=a
 		.pipe(gulp.dest(paths.testDirectory));
 
-	// gulp.run('livereload');
-	// gulp.run('jasmine');
 	return a;
 });
 
